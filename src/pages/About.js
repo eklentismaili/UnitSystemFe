248506eba5947902df8cc35adfa0d61105bbb944
Eklent Ismaili <ismailieklent3@gmail.com>
2022-05-12 17:38:51 +0200

    Added styling fix to routes height

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div className="min-height-100vh">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">home</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="min-height-100vh">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">home</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
