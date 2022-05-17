7675419aa19352f426c2c84b154952851974605e
Eklent Ismaili <ismailieklent3@gmail.com>
2022-05-17 16:38:56 +0200

    Added filter tasks based on complition

--- src/pages/Tasks/Tasks.js
+++ src/pages/Tasks/Tasks.js
@@ -47,6 +47,10 @@ function Tasks() {
     },
   ];
 
+  const [filter, setFilter] = useState({
+    completed: "all",
+  });
+
   const [pageData, setPageData] = useState({
     rowData: [],
     isLoading: false,
@@ -66,31 +70,64 @@ function Tasks() {
       isLoading: true,
     }));
     getTasksPagination(currentPage);
-  }, [currentPage]);
+  }, [currentPage, filter]);
 
   const getTasksData = (pageNo, totalTasks, totalPages) => {
-    axios
-      .get(`http://localhost:8000/users/${id}/tasks?_page=${pageNo}&_limit=6`)
-      .then((info) => {
-        const data = info.data;
+    if (filter.completed === "all") {
+      axios
+        .get(`http://localhost:8000/users/${id}/tasks?_page=${pageNo}&_limit=6`)
+        .then((info) => {
+          const data = info.data;
 
-        setPageData({
-          ...pageData,
-          totalPages: totalPages,
-          totalTasks: totalTasks,
-          isLoading: false,
-          rowData: formatRowData(data),
+          setPageData({
+            ...pageData,
+            totalPages: totalPages,
+            totalTasks: totalTasks,
+            isLoading: false,
+            rowData: formatRowData(data),
+          });
         });
-      });
+    } else {
+      axios
+        .get(
+          `http://localhost:8000/users/${id}/tasks?_page=${pageNo}&_limit=6&completed=${filter.completed}`
+        )
+        .then((info) => {
+          const data = info.data;
+
+          setPageData({
+            ...pageData,
+            totalPages: totalPages,
+            totalTasks: totalTasks,
+            isLoading: false,
+            rowData: formatRowData(data),
+          });
+        });
+    }
   };
 
   const getTasksPagination = (pageNo) => {
-    axios.get(`http://localhost:8000/tasks?userId=${id}`).then(async (info) => {
-      const totalPages = Math.ceil(info.data.length / 6);
-      const totalTasks = info.data.length;
+    if (filter.completed === "all") {
+      axios
+        .get(`http://localhost:8000/tasks?userId=${id}`)
+        .then(async (info) => {
+          const totalPages = Math.ceil(info.data.length / 6);
+          const totalTasks = info.data.length;
 
-      getTasksData(pageNo, totalTasks, totalPages);
-    });
+          getTasksData(pageNo, totalTasks, totalPages);
+        });
+    } else {
+      axios
+        .get(
+          `http://localhost:8000/tasks?userId=${id}&completed=${filter.completed}`
+        )
+        .then(async (info) => {
+          const totalPages = Math.ceil(info.data.length / 6);
+          const totalTasks = info.data.length;
+
+          getTasksData(pageNo, totalTasks, totalPages);
+        });
+    }
   };
 
   const onCreate = () => {
@@ -115,6 +152,25 @@ function Tasks() {
               Total Tasks: {pageData.totalTasks}
             </span>
             <CreateTask onCreate={onCreate} userId={id} />
+          </div>
+          <div className="col-12">
+            <div className="filter">
+              <span>Filter tasks by completed:</span>
+              <select
+                class="form-select"
+                value={filter.completed}
+                onChange={(value) =>
+                  setFilter({
+                    ...filter,
+                    completed: value.target.value,
+                  })
+                }
+              >
+                <option value={"all"}>All</option>
+                <option value={true}>True</option>
+                <option value={false}>False</option>
+              </select>
+            </div>
           </div>
           <div className="col-12">
             {pageData.isLoading ? (
@@ -151,5 +207,5 @@ const formatRowData = (rawData) =>
     id: info.id,
     userId: info.userId,
     title: info.title,
-    completed: info.completed === "true" ? "true" : "false",
+    completed: info.completed.toString(),
   }));
